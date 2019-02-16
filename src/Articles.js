2606970451d405212b8447532565e5233f79f4e1
Wayne Ferrao <wayne.ferrao93@gmail.com>
2019-02-16 02:41:07 -0500

    lolz

--- src/Articles.js
+++ src/Articles.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Header from './header';
+import TopBar from "./topbar";
 
 
 export default class Articles extends Component {
@@ -8,6 +10,8 @@ export default class Articles extends Component {
     render() {
         return (
             <div>
+                <Header/>
+                <TopBar/>
                 <h1>
                     Articles
                 </h1>
