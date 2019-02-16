e34f1bc17a898cb9531967b6f3c08899159ff375
Wayne Ferrao <wayne.ferrao93@gmail.com>
2019-02-16 15:21:26 -0500

    Update header.js

--- src/header.js
+++ src/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Hidden from '@material-ui/core/Hidden';
 
-
+import Typography from '@material-ui/core/Typography';
 
 import Grid from '@material-ui/core/Grid';
 
