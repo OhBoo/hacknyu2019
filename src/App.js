9ceb034b7495fbd599b448bce23eabe99083fadb
Elizabeth Labor <em.labor@nyu.edu>
2019-02-16 05:03:11 -0500

    updated home

--- src/App.js
+++ src/App.js
@@ -7,11 +7,26 @@ import Forum from './Forum';
 import Header from './header';
 import Topbar from './topbar.js';
 import Home from './home.js';
-const Container = styled.div`
+import Grid from '@material-ui/core/Grid';
+import {Parallax, Background} from 'react-parallax';
+
+const Container = styled.div`	
 
 `;
-const BoxContainer = styled.div`
-`
+
+const Banner = styled.div`
+	background-image: url("https://images.unsplash.com/photo-1513625047154-f390b1b0df20?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
+	background-size: cover;
+	background-repeat: no-repeat;
+	
+`;
+
+const Message = styled.div`
+	text-align: center;
+	font-color: white;
+`;
+
+
 
 export default class App extends Component {
     constructor(props) {
@@ -23,9 +38,26 @@ export default class App extends Component {
             <Container>
                 <Header/>
                 <Topbar/>
+                
+                <Parallax>
+            	<Banner>
+            	<Grid container spacing = {12}>
+            		<Message>
+            		<h4>
+						Welcome
+						</h4>
+					</Message>
+				</Grid>
+					<div style={{height: '500px'}}/>
+            	</Banner>
+            	</Parallax>
+            	
             </Container>
 
+
+
         );
+
         return (
             <Router>
                 <div>
