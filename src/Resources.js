b2999b5fa63421259349c3253625c63d5133719b
saustinw@hotmail.com <m@92Tf2S6TbzXXL^>
2019-02-16 04:13:10 -0500

    add basic linking in Resources

--- src/Resources.js
+++ src/Resources.js
@@ -4,10 +4,11 @@ import Header from "./header";
 import TopBar from "./topbar";
 
 const Title = styled.h1`
-font-size: 50px;    
-
+font-size: 35px;
 `;
-
+const Resource = styled.h2`
+font-size: 25px;
+`;
 
 export default class Resources extends Component {
 
@@ -17,8 +18,27 @@ export default class Resources extends Component {
                 <Header/>
                 <TopBar/>
                 <Title>
-                    Look at all these resources
+                    Connecting YOU with the Resources YOU Need
                 </Title>
+
+                <Resource>
+                    <a href="https://www1.nyc.gov/assets/doh/downloads/pdf/std/teens-clinics.pdf">Teen Clinics</a>
+                </Resource>
+                <Resource>
+                    <a href="https://hmi.org/">Hetrick-Martin Institute</a>
+                </Resource>
+                <Resource>
+                    <a href="https://www.plannedparenthood.org/online-tools/what-right-birth-control-me">Planned Parenthood</a>
+                </Resource>
+                <Resource>
+                    <a href="https://heyepiphora.com/">Hey Epiphora</a>
+                </Resource>
+                <Resource>
+                    <a href="https://vaginaantics.com/vagina-antics-blog/">Vagina Antics</a>
+                </Resource>
+                <Resource>
+                    <a href="https://www.cindyleealves.com/">Cindy Lee Alves</a>
+                </Resource>
             </div>
         )
     }
